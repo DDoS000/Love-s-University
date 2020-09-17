90a248d9def979b7cda96b48f65a60945d304aec
DDoS000 <Suphamongkhon.kh.61@ubu.ac.th>
2020-09-17 17:23:40 +0700

    fix db

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ MYSQL_DB            = 'University'
 
 
 # connection MySQL
-connection = pymysql.connect(host=MYSQL_HOST, user=MYSQL_USER, password=MYSQL_PASSWORD, db=MYSQL_DB , cursorclass=pymysql.cursors.DictCursor)
+# connection = pymysql.connect(host=MYSQL_HOST, user=MYSQL_USER, password=MYSQL_PASSWORD, db=MYSQL_DB , cursorclass=pymysql.cursors.DictCursor)
 
 
 # Index
